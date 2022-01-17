de8e564d93234a363e790d739a8535fd1c23daed
XieGuochao <117010307@link.cuhk.edu.cn>
2022-01-17 16:40:15 +0000

    hello world example

---
+++ bcc/hello_world_cgroup_mkdir.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# hello_world_cgroup_mkdir.py
+# Author: Guochao
+# Created on 17-01-2022
+
+# Print hello world on each cgroup mkdir
+
+from bcc import BPF
+
+program = """
+int kprobe__cgroup_mkdir(void *ctx) {
+    bpf_trace_printk("hello world\\n");
+    return 0;
+}
+"""
+
+BPF(text=program).trace_print()
