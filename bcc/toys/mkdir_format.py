5ff82d3498a52fd68addd19e617e70aecbe4d4c1
XieGuochao <117010307@link.cuhk.edu.cn>
2022-01-21 21:27:47 +0000

    better version of mkdir_format

--- bcc/toys/mkdir_format.py
+++ bcc/toys/mkdir_format.py
@@ -12,6 +12,7 @@ from bcc import BPF
 program = r"""
 #include <uapi/linux/ptrace.h>
 #include <linux/kernfs.h>
+#include <linux/sched.h>
 
 BPF_HASH(start, u32);
 BPF_PERF_OUTPUT(events);
@@ -19,9 +20,12 @@ BPF_PERF_OUTPUT(events);
 struct data_t {
     u64 ts;
     u64 delta;
+    u32 ret;
+    char comm[TASK_COMM_LEN];
+    char name[100];
 };
 
-TRACEPOINT_PROBE(cgroup, cgroup_mkdir) {
+KFUNC_PROBE(cgroup_mkdir) {
     // args is from /sys/kernel/debug/tracing/events/cgroup/cgroup_mkdir/format
     // although we are not using here.
 
@@ -32,17 +36,20 @@ TRACEPOINT_PROBE(cgroup, cgroup_mkdir) {
     return 0;
 }
 
-int kretprobe__cgroup_mkdir(struct pt_regs *ctx) {
+KRETFUNC_PROBE(cgroup_mkdir, struct kernfs_node *parent_kn, const char *name, umode_t mode, int ret) {
     u64 *tsp, delta;
     u64 ts = bpf_ktime_get_ns();
     u32 pid = bpf_get_current_pid_tgid();
     struct data_t data = {};
     data.ts = ts;
+    data.ret = ret;
 
     tsp = start.lookup(&pid);
     if (tsp != 0) {
         delta = bpf_ktime_get_ns() - *tsp;
         data.delta = delta;
+        bpf_probe_read_kernel_str(data.name, 100, name);
+        bpf_get_current_comm(&data.comm, sizeof(data.comm));
         events.perf_submit(ctx, &data, sizeof(data));
         start.delete(&pid);
     }
@@ -54,7 +61,8 @@ b = BPF(text=program)
 
 print("Start tracing")
 
-
+format="%6.2f %10d     %3d %-20s %s"
+print("%6s %10s     %3s %-20s %s" % ("time", "lat(us)", "ret", "name", "command"))
 start = 0
 def print_event(cpu, data, size):
     global start
@@ -62,7 +70,7 @@ def print_event(cpu, data, size):
     if start == 0:
         start = event.ts
     time_s = (float(event.ts - start)) / 1000000000
-    print("At time %.2f s: %10d us" % (time_s, event.delta // 100))
+    print(format % (time_s, event.delta // 100, event.ret, event.name.decode("utf8"), event.comm.decode("utf8")))
 
 b["events"].open_perf_buffer(print_event)
 while 1:
